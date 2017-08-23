08b3b72f8c1b27d3c37ddeccfac4b879d5d86fbe
staplemx <staplemx@gmail.com>
2017-08-23 05:00:22 -0700

    starting to build

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='testAPI',
+    version='',
+    packages=['testAPI'],
+    url='',
+    license='LICENSE',
+    author='staplemx',
+    author_email='staplemx@gmail.com',
+    description=''
+)
